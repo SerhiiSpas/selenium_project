e88775b0d52c54d18ce12ad9aadf232fb54681bb
SerhiiSpas <serhiispas@gmail.com>
2020-03-30 11:06:57 +0300

    foto

--- test_google.py
+++ test_google.py
@@ -1,6 +1,7 @@
 def test_google():
     import time
     from selenium import webdriver
+    from selenium.webdriver.support.wait import WebDriverWait
     driver = webdriver.Chrome('chromedriver.exe')
     driver.get('https://www.google.com/')
 
@@ -14,8 +15,23 @@ def test_google():
     password.send_keys('257992ss')
     entrance = driver.find_element_by_xpath('//*[@id="u_0_b"]')
     entrance.click()
-    button_spas = driver.find_element_by_xpath('//*[@id="mount_0_0"]/div/div/div[1]/div[4]/div/div/span/div/div')
-    button_spas.click()
-    acount = driver.find_element_by_xpath('//*[@id="jsc_c_d6"]/div/div[2]/span')
-    acount.click()
+
+    def button_wait(driver):
+        button_spas = driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[4]/div/div/span/div')
+        button_spas.click()
+        return (button_spas)
+
+    WebDriverWait(driver, 5).until(button_wait)
+
+    def profil_wait(driver):
+        acount = driver.find_element_by_xpath(
+            '//*[@id="mount_0_0"]/div/div/div[1]/div[4]/div/span/span/div[1]/div/div/div/div/div/div/div/div/div/div[1]/div[1]')
+        acount.click()
+        return (acount)
+
+    WebDriverWait(driver, 5).until(profil_wait)
+
+    foto = driver.find_element_by_xpath('/html/body/div[1]/div/div/div[2]/div/div/div/div/div/div/div/div[4]/div/div[2]/div/div[1]/div/div/div/div[2]/div/div/div[1]/div/div/h1/div/div[1]/span/div/a')
+    foto.click()
+
     print(None)
